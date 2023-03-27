6feba18533606c076feaf2f38242d2219653823f
Gurman-Singh-03 <singhgurman573@gmail.com>
2023-03-27 18:49:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 require('dotenv').config()
 
 app.get('/', (req, res) => {
-    res.send("hello from node updated 3")
+    res.send("hello from nodemon ")
 });
 
 const port = process.env.PORT ||8000
